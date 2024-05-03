b587cb28124991916f913e3c4967f03275621609
Paul Serrano <paul.serrano08374@gmail.com>
2024-05-03 15:17:44 +0200

    STYLE: band design DONE

--- red_wire_app/src/app/components/band/band.component.ts
+++ red_wire_app/src/app/components/band/band.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-band',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './band.component.html',
   styleUrl: './band.component.css'
 })
 export class BandComponent implements OnInit {
+  logged:boolean = false;
 
   constructor(private auth: AuthService) {
 
   }
 
   ngOnInit(): void {
-    
+    const loggedInUser = sessionStorage.getItem('loggedInUser');
+    if(loggedInUser) this.logged = true;
   }
 
   signOut(): void {
