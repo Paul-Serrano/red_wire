63d1c62ded7bfe205ae05ac0a42884b25fc1362b
Serrano Paul <83453497+Paul-Serrano@users.noreply.github.com>
2024-05-05 13:30:32 +0200

    Delete red_wire_app directory

--- red_wire_app/start.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-// Exécute le script Python
-const pythonProcess = exec("npm run start:app");
-
-// Lorsque le script Python est terminé, démarre l'application Angular
-pythonProcess.on("close", () => {
-  exec("npm run start:angular");
-});
